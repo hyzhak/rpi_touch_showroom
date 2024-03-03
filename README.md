7b64174186351e1a27bcd9109abe07e8933d094c
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2024-03-03 00:52:35 -0800

    feat: visualize all touches on a screen with different colors

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# rpi_touch_showroom
+# RPI Touch ShowRoom
+
+## Description
+Show room for the RPI Touch Screen abilities of the Raspberry Pi.
+
+## Setup for SSH execution
+
+```sh
+export DISPLAY=:0
+```
+
+## Run
+
+```sh
+python rpi_touch_showroom/__main__.py
+```
---
+++ rpi_touch_showroom/__main__.py
@@ -0,0 +1,46 @@
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.display.set_caption("Touch Experiment")
+
+
+def hsva_color(h, s, v, a):
+    c = pygame.Color(0, 0, 0, 0)
+    c.hsva = (h, s, v, a)
+    return c
+
+
+colors = [hsva_color(h, 100, 50, 100) for h in range(0, 360, 360 // 32)]
+
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+x, y = 0, 0
+fingers = {}
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.FINGERDOWN:
+            x = event.x * screen.get_width()
+            y = event.y * screen.get_height()
+            fingers[event.finger_id] = x, y
+
+        if event.type == pygame.FINGERMOTION:
+            x = event.x * screen.get_width()
+            y = event.y * screen.get_height()
+            fingers[event.finger_id] = x, y
+
+        if event.type == pygame.FINGERUP:
+            fingers.pop(event.finger_id, None)
+
+    screen.fill((0, 0, 0))
+
+    for finger_id, (x, y) in fingers.items():
+        pygame.draw.circle(screen, colors[finger_id % len(colors)], (x, y), 50)
+
+    pygame.display.update()
